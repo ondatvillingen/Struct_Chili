8febe1d74f2caf8c700b799f3f58cb5a2f5b97ad
Ondatvillingen <christopher.m.johansson@gmail.com>
2013-05-05 21:17:30 +0200

    fixed While loop

--- SerialPortReader.py
+++ SerialPortReader.py
@@ -1,4 +1,14 @@
 import serial
-serialport = serial.Serial("/dev/tty.usbmodem411", 9600, timeout= 2)
+
+# change serial.port name to: ttyAMA0 for raspberry pi
+
+while True:
+	try: 
+		serialport = serial.Serial("/dev/tty.usbmodem411", 9600, timeout= 0.5)
+		break
+	except: 
+		print "Serial Error"
+		exit(0)
 response = serialport.readlines(None)
 print response
+
