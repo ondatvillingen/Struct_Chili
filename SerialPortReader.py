2025de2b3d9833a8239171aa48f21bae9aa49c81
Ondatvillingen <christopher.m.johansson@gmail.com>
2013-06-30 19:45:21 +0200

    works as a Serial Port Reader with Arduino code "SerialWrite2" on Raspberry

--- SerialPortReader.py
+++ SerialPortReader.py
@@ -1,14 +1,4 @@
 import serial
-
-# change serial.port name to: ttyAMA0 for raspberry pi
-
+ser = serial.Serial('/dev/ttyACM0' , 9600)
 while True:
-	try: 
-		serialport = serial.Serial("/dev/tty.usbmodem411", 9600, timeout= 0.5)
-		break
-	except: 
-		print "Serial Error"
-		exit(0)
-response = serialport.readlines(None)
-print response
-
+	print ser.readline()
